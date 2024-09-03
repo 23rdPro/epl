36db3b2c002821c9d3f8a63fe897cb6ad36f566e
23rdPro <mailolumide@gmail.com>
2024-09-03 21:31:20 +0100

    added get fixture view

--- epl_api/urls.py
+++ epl_api/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from fastapi import APIRouter, status
-
-from epl_api.v1.schema import PlayerStatsSchema
-from epl_api.views import get_root, get_p_stats, get_table
+from epl_api.views import get_fixtures, get_root, get_p_stats, get_table
 
 router = APIRouter()
+
 router.get("/", status_code=status.HTTP_200_OK)(get_root)
 router.get("/stats/{p_name}", status_code=status.HTTP_200_OK, summary="get player stats", tags="pl-stats")(get_p_stats)
 router.get("/table", status_code=status.HTTP_200_OK, summary="get epl table", tags="epl-table")(get_table)
+router.get("/fixtures", status_code=status.HTTP_200_OK, summary="", tags="epl-fixtures")(get_fixtures)
