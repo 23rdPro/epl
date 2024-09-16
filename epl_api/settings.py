920d36c2a11eec4126ba7ee9254748076f387da3
23rdPro <mailolumide@gmail.com>
2024-09-16 11:01:09 +0100

    minor change

--- epl_api/settings.py
+++ epl_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
@@ -125,7 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# settings.py
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
