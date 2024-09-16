b838766f4bb216103c3f1bd40b2fbb728a6b523e
23rdPro <mailolumide@gmail.com>
2024-09-16 16:40:44 +0100

    added vercel config

--- epl_api/asgi.py
+++ epl_api/asgi.py
@@ -30,7 +30,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Can limit to specific domains
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
