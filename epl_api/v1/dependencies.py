f42272f69f2199b322222c16d26d0dd906c4d3dd
23rdPro <mailolumide@gmail.com>
2024-09-18 22:28:14 +0100

    minor

--- epl_api/v1/dependencies.py
+++ epl_api/v1/dependencies.py
@@ -12,14 +12,31 @@ from playwright.async_api import async_playwright
 #             await browser.close()
 
 
+# async def get_page():
+#     browser = None
+#     try:
+#         async with async_playwright() as p:
+#             browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
+#             page = await browser.new_page()
+#             yield page
+#     except Exception as e:
+#         logging.error(f"Failed to launch Playwright: {e}")
+#         raise
+#     finally:
+#         await browser.close()
+
+
 async def get_page():
-    try:
-        async with async_playwright() as p:
+    browser = None  
+    async with async_playwright() as p:
+        try:
             browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
             page = await browser.new_page()
-            yield page
-    except Exception as e:
-        logging.error(f"Failed to launch Playwright: {e}")
-        raise
-    finally:
-        await browser.close()
+            try:
+                yield page
+            finally:
+                if browser:  
+                    await browser.close()
+        except Exception as e:
+            logging.error(f"Failed to launch Playwright: {e}")
+            raise
